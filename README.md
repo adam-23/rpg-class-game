e96a48971bb298df901b2ac95bd9055df5709ea4
Adam <theadam43@gmail.com>
2016-12-29 18:12:53 -0700

    Initial upload. Hope this works.

---
+++ README.md
@@ -0,0 +1,2 @@
+# class-game
+Turn based strategic RPG built on teams and the class system. 
---
+++ dark-knight-class.py
@@ -0,0 +1,44 @@
+class Job:
+    def __init__(self):
+        # Dark Knight Class
+        """
+        self.vitality = vitality
+        self.attack = attack
+        self.defense = defense
+        self.int = intelligence
+        self.spirit = spirit
+        self.speed = speed
+
+        type modifier
+        weapons
+        armor
+        special ability
+        magic types
+        stat modifiers
+
+        range
+        multi or single target
+
+        """
+
+    # Defaults
+    """
+    Vit = 13
+    Attack = 13
+    Defense = 11
+    Int = 11
+    Spirit = 5
+    Speed = 7
+
+
+    # Specials
+    Soul eater
+    Blood blade
+    Dark Lance
+    Mind Drain
+    Black Orb
+    Obscure
+    Darken
+    Cold Grip
+    """
+    
